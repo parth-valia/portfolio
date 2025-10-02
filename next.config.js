34c3c9a6073954dcf04f1f725b663b87a1728218
Muskan <muskan.sahu@corenettechltd.com>
2025-10-02 18:06:28 +0530

    fix dynamic route error

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   eslint: { ignoreDuringBuilds: true },
   trailingSlash: true,
   images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
+
